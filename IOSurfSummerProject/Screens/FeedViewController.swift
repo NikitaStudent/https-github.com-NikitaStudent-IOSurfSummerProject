8684036b1c80d57b5d2e97f2123b0246012e86f5
NikitaStudent <NKA_2000_00@mail.ru>
2019-08-09 22:28:57 +0300

    распределили файлы по папкам и Screens, Application, Resources и добавили FeedViewConrtoller файл отвечающий за отображение в TableView

---
+++ IOSurfSummerProject/Screens/FeedViewController.swift
@@ -0,0 +1,9 @@
+//
+//  FeedViewController.swift
+//  IOSurfSummerProject
+//
+//  Created by Никита Кожевников on 09/08/2019.
+//  Copyright © 2019 Beet inc. All rights reserved.
+//
+
+import UIKit
