f85b7591ef03e531438636b034aa8835c3203190
NikitaStudent <NKA_2000_00@mail.ru>
2019-08-09 20:48:04 +0300

    Initial Commit

---
+++ IOSurfSummerProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSurfSummerProject
+//
+//  Created by Никита Кожевников on 09/08/2019.
+//  Copyright © 2019 Beet inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
